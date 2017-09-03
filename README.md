bee584b8aa7ea648f459819268f3746d7e9c715c
Kubosaurus <31464856+Kubosaurus@users.noreply.github.com>
2017-09-03 03:51:26 -0700

    Update bot.js
    HOOMAN "can" report the weather

--- README.md
+++ README.md
@@ -3,23 +3,10 @@ Basic bot for Discord Server
 
 Name: HOOMAN
 
-Version: 1.0.0
-
-Description: This [bot] is a test bot for my discord server and as a practice
-to making bots-desu.
-Version 1.0.0 will start as a simple chat bot.
-
-Current: Nothing...
-
-Future plans: New capabilities [play music, etc]
-
 main: bot.js
 
 Author: David Okubo
 
-Collabs: NONE, for now...
-
-Dependencies:
 
 Sources:
 Code based on Renemari Padillo's tutorial: https://medium.com/@renesansz/tutorial-creating-a-simple-discord-bot-9465a2764dc0
--- bot.js
+++ bot.js
@@ -1,29 +1,66 @@
+/*********************************************************
+**********************Dependencies************************
+**********************************************************/
 var Discord = require('discord.io');
 var auth = require('./auth.json');
+const http = require('http'); //node.js http requests
 
+/*********************************************************
+*********************Global Variables*********************
+**********************************************************/
+var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
+var weatherUnits = "&units=imperial&appid=";
+var appId = auth.appid;
+
+
+//create bot using its authentication token
 var bot = new Discord.Client({
 	autorun: true,
 	token: auth.token
 });
 
+
+//log bot into Discord - prints log in to command line window
 bot.on('ready', function(event) {
 	console.log('Logged in as %s - %s\n', bot.username, bot.id);
 });
 
+//Bot responses on the server
 bot.on('message', function(user, userID, channelID, message, event){
-	if(message.substring(0,1) == '!'){
-		var args = message.substring(1).split(' ');
+	if(message.substring(0,1) == '!'){				//Will check and respond to messages
+		var args = message.substring(1).split(' '); // starting with an "!"
 		var check = args[0];
+		var city = args[1];
 		if (check === 'Hello' || check === 'hello' || check === 'HELLO' ){
 			bot.sendMessage ({
 				to: channelID,
 				message: "Hello, " + user + "! \:grinning:"
 			});
 		}
-		if (check === 'HOOMAN' || check === 'Hooman' || check === 'hooman'){
+		else if (check === 'HOOMAN' || check === 'Hooman' || check === 'hooman'){
 			bot.sendMessage ({
 				to: channelID,
 				message: "HOOMAN-desu!"
+			});
+		}
+		else if (check === 'weather' || check === 'Weather' || check === 'WEATHER'){
+			if(city === undefined){
+				city = "corvallis";
+			}
+			var weather = weatherAPI.concat(city, weatherUnits, appId);
+			console.log(typeof(weather));
+			var body = ' ';
+			var req = http.get(weather, function(res){
+				res.on('data', function(chunk) {
+					body += chunk.toString();
+				});
+				res.on('end', function(){
+					data = JSON.parse(body);
+					bot.sendMessage({
+						to: channelID,
+						message: "The temperature in " + data.sys.name + " is: " + data.main.temp
+					});
+				});
 			});
 		}
 	}
